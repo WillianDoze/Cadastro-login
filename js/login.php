a7abd23c975d75ee9f70c4b7e282ac66f241ff19
Willian da Silva Ferreira <104800185+WillianDoze@users.noreply.github.com>
2023-02-16 10:14:36 -0300

    att

---
+++ js/login.php
@@ -0,0 +1,44 @@
+<?php
+ 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "cadastro_cliente";
+ 
+// Create connection
+$conn = new mysqli($servername,
+    $username, $password, $dbname);
+ 
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: "
+        . $conn->connect_error);
+}
+
+$sqlquery = "INSERT INTO clientes VALUES
+    ('$_POST[nome]', '$_POST[CPF]', '$_POST[RG]', '$_POST[CEP]', '$_POST[Endereco]', '$_POST[Cidade]', '$_POST[Estado]', '$_POST[Numero]', '$_POST[Email]', '','')";
+ 
+if ($conn->query($sqlquery) === TRUE) {
+    echo "record inserted successfully";
+} else {
+    echo "Error: " . $sqlquery . "<br>" . $conn->error;
+}
+
+// Closing the connection.
+$conn->close();
+ 
+?>
+
+
+ 
+<!-- // if ($_SERVER["REQUEST_METHOD"] == "POST") {
+ 
+//     // collect value of input field
+//     $data = $_REQUEST['val1'];
+ 
+//     if (empty($data)) {
+//         echo "data is empty";
+//     } else {
+//         echo $data;
+//     }
+// } -->
